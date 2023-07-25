bebf6bb4899a15f8adbd87ec37378dc319eade93
david-hp-0726 <xche653@emory.edu>
2023-07-25 10:32:26 +0800

    Initial Commit

---
+++ Wordle/WordleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordleApp.swift
+//  Wordle
+//
+//  Created by David Chen on 2023/7/25.
+//
+
+import SwiftUI
+
+@main
+struct WordleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
